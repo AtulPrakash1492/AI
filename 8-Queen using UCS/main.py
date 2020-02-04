f3e7e5848226bfa17499d0d34a63cc4a89220f0e
AtulPrakash1492 <57394759+AtulPrakash1492@users.noreply.github.com>
2020-02-04 17:10:31 +0530

    Create main.py

---
+++ 8-Queen using UCS/main.py
@@ -0,0 +1,17 @@
+from n_queens import NQueens
+
+
+def main():
+    print('******* N-Queens Problem *******')
+    size = int(input('Number of Queens: '))
+    print_solutions = 'true'
+    n_queens = NQueens(size)
+    ucs_solutions = n_queens.solve_ucs()
+    if print_solutions:
+        for i, solution in enumerate(ucs_solutions):
+            print('UCS Solution %d:' % (i + 1))
+            n_queens.print(solution)
+    print('Total UCS solutions: %d' % len(ucs_solutions))
+
+if __name__ == '__main__':
+    main()
